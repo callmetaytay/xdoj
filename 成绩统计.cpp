39e5ba9a28f694f365d9b4e66c68340b5f27fc28
callmetaytay <145259471+callmetaytay@users.noreply.github.com>
2024-01-07 18:18:50 +0800

    Add files via upload

--- 成绩统计.cpp
+++ 成绩统计.cpp
@@ -1,31 +1,36 @@
 #include<iostream>
-#include<string>
 #include<vector>
+#include<string>
 #include<algorithm>
 #include<iomanip>
 using namespace std;
 struct student{
-	string num;
-	double score;
-	student():num(" "),score(0.0){
+	int num;
+	string name;
+	double sum;
+	student():num(0),name(" "),sum(0.0){
 	}
 };
 bool compare(student &a,student &b){
-	if(a.score!=b.score)return a.score<b.score;
+	if(a.sum!=b.sum)return a.sum>b.sum;
+	else return a.num<b.num;
 }
 int main()
 {
 	int n;
 	cin>>n;
 	vector<student> students(n);
-	double sum=0.0;
 	for(int i=0;i<n;i++){
-		cin>>students[i].num>>students[i].score;
-		sum+=students[i].score;
+		cin>>students[i].num>>students[i].name;
+		for(int j=0;j<3;j++){
+			int x;
+			cin>>x;
+			students[i].sum+=x;
+		}
+		students[i].sum/=3.0;
 	}
-	sort(students.begin(),students.end(),compare);
-	cout<<fixed<<setprecision(2)<<students[n-1].score<<' '<<students[n-1].num<<endl;
-	cout<<fixed<<setprecision(2)<<students[0].score<<' '<<students[0].num<<endl;
-	cout<<fixed<<setprecision(2)<<sum<<' '<<fixed<<setprecision(2)<<sum/n;
-}
-
+/*	sort(students.begin(),students.end(),compare);
+	for(int i=0;i<n;i++){
+		cout<<students[i].num<<' '<<students[i].name<<' '<<fixed<<setprecision(1)<<students[i].sum;
+	}*/
+ } 
